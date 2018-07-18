3a36109766365680980764333fd0de9ab66e408e
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-07-18 23:07:32 +0000

    automatically backed up by learn

--- bin/kickstarter_scraper.rb
+++ bin/kickstarter_scraper.rb
@@ -20,7 +20,7 @@ def create_project_hash
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcardblurb").text
       :location =>  project.css("ul.project-meta span.location-name").text
-      :percent_funded => 
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
   projects
