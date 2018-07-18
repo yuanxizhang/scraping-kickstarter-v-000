792e01020df6df40d0afd3f2703ccde45e13f72e
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-07-18 21:48:33 +0000

    automatically backed up by learn

--- bin/kickstarter_scraper.rb
+++ bin/kickstarter_scraper.rb
@@ -8,10 +8,11 @@ kickstarter = Nokogiri::HTML(html)
 
 # projects: kickstarter.css("li.project.grid_4")
 projects = []
-projects << kickstarter.css("li.project grid_4").text
-projects.each 
-project = {}
-project[title] = kickstarter.css("h2.bbcard_name strong a").text
+projects << kickstarter.css("li.project grid_4")
+projects.each |p| do
+  project = {}
+  project[title] = kickstarter.css("h2.bbcard_name strong a").text
+end  
 puts kickstarter.css("li.project grid_4").first
 
 binding.pry
