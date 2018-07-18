5355f3e7ba5fd44a2e6184cd98e073f856c45813
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-07-18 23:17:45 +0000

    automatically backed up by learn

--- bin/kickstarter_scraper.rb
+++ bin/kickstarter_scraper.rb
@@ -20,8 +20,8 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcardblurb").text
-      :location =>  project.css("ul.project-meta span.location-name").text
+      :description => project.css("p.bbcardblurb").text,
+      :location =>  project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
