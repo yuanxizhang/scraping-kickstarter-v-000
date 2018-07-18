11d5b88fbaf4dbedd65f690e27dda900f7303f86
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-07-18 23:09:34 +0000

    automatically backed up by learn

--- bin/kickstarter_scraper.rb
+++ bin/kickstarter_scraper.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def create_project_hash
   #read the HTML file 
-  html = File.read('../fixtures/kickstarter.html') 
+  html = File.read('./fixtures/kickstarter.html') 
   kickstarter = Nokogiri::HTML(html)
   
   projects = {}
