725b465fef4956bfa749b9dc1d1422e9cfff2879
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-07-18 23:01:38 +0000

    automatically backed up by learn

--- bin/kickstarter_scraper.rb
+++ bin/kickstarter_scraper.rb
@@ -16,7 +16,7 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project grid_4").each do |project| 
     title = project.css("h2.bbcard_name strong a").text
-    projects[project] = {}
+    projects[title.to_sym] = {}
   end
   projects
 end 
